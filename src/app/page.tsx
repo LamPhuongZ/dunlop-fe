5e49afe4411e0c90d910f2bf528d520066c064cb
NguyenHaAnh <haanhnguyen1221@gmail.com>
2025-08-02 01:06:04 +0700

    feat: add hero images and update layout with Header and Footer components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import { Footer, Header } from "@/components/common";
-import { DunlopTypeSection, HeroSection } from "@/components/pages/home";
+import { DunlopTypeSection, HeroSection } from '@/components/pages/home';
 
 export default function Home() {
   return (
     <>
-      <Header />
       <HeroSection />
       <DunlopTypeSection />
-      <Footer />
     </>
   );
 }
