0a58c7d2602ca84b1192142c5e675635b1b6e9d0
NguyenHaAnh <haanhnguyen1221@gmail.com>
2025-08-19 19:59:34 +0700

    feat: add About Us page and update header links

---
+++ src/components/pages/about-us/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Đây là trang giới thiệu về công ty...</p>
+    </div>
+  );
+};
+
+export default AboutUs;
